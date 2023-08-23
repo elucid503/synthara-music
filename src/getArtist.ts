fd6fc308626e82b28757446e68f80521f5f022cd
Elucid503 <paulujlaky@gmail.com>
2023-08-22 20:43:38 -0400

    Switch from Got to Node Fetch

--- src/getArtist.ts
+++ src/getArtist.ts
@@ -1,4 +1,4 @@
-import got from 'got';
+import fetch from 'node-fetch';
 import context from './context.js';
 import { Artist } from './models.js';
 import { parseArtistData } from './parsers.js';
@@ -10,13 +10,14 @@ export async function getArtist(
     country: string;
   }
 ): Promise<Artist> {
-  const response = await got.post(
+  const response = await fetch(
     'https://music.youtube.com/youtubei/v1/browse?key=AIzaSyC9XL3ZjWddXya6X74dJoCTL-WEYFDNX30',
     {
-      json: {
+      method: "POST",
+      body: JSON.stringify({
         ...context.body,
         browseId: artistId,
-      },
+      }),
       headers: {
         'User-Agent':
           'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
@@ -27,7 +28,13 @@ export async function getArtist(
   );
 
   try {
-    return parseArtistData(JSON.parse(response.body), artistId);
+    let JsonData = await response.json();
+    
+    if (!JsonData) { 
+      throw new Error('No data returned from YouTube Music API');
+    }
+
+    return parseArtistData(JsonData as any, artistId);
   } catch (e) {
     console.error(e);
     return {};
