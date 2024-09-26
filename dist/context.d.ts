0b05bd365c86f099923a8982ea45cb3dd300fbc2
elucid503 <paulujlaky@gmail.com>
2024-09-25 21:48:02 -0400

    Fix youtube blocking requests based on bad context

--- dist/context.d.ts
+++ dist/context.d.ts
@@ -3,10 +3,34 @@ declare const _default: {
         context: {
             capabilities: {};
             client: {
+                browserName: string;
+                browserVersion: string;
+                clientFormFactor: string;
                 clientName: string;
                 clientVersion: string;
-                hl: string;
+                deviceMake: string;
+                deviceModel: string;
                 gl: string;
+                hl: string;
+                originalUrl: string;
+                osName: string;
+                osVersion: string;
+                platform: string;
+                screenDensityFloat: number;
+                screenHeightPoints: number;
+                screenPixelDensity: number;
+                screenWidthPoints: number;
+                timeZone: string;
+                userAgent: string;
+                userInterfaceTheme: string;
+                utcOffsetMinutes: number;
+            };
+            request: {
+                consistencyTokenJars: never[];
+                internalExperimentFlags: never[];
+            };
+            user: {
+                lockedSafetyMode: boolean;
             };
         };
     };
--- dist/context.js
+++ dist/context.js
@@ -2,12 +2,36 @@ export default {
     body: {
         context: {
             capabilities: {},
-            'client': {
-                'clientName': 'WEB_REMIX',
-                'clientVersion': '0.1',
-                'hl': 'en',
-                'gl': 'US',
+            "client": {
+                "browserName": "Firefox",
+                "browserVersion": "128.0",
+                "clientFormFactor": "UNKNOWN_FORM_FACTOR",
+                "clientName": "WEB_REMIX",
+                "clientVersion": "1.20240715.01.00",
+                "deviceMake": "",
+                "deviceModel": "",
+                "gl": "US",
+                "hl": "en",
+                "originalUrl": "https://music.youtube.com",
+                "osName": "Windows",
+                "osVersion": "10.0",
+                "platform": "DESKTOP",
+                "screenDensityFloat": 1,
+                "screenHeightPoints": 679,
+                "screenPixelDensity": 1,
+                "screenWidthPoints": 1740,
+                "timeZone": "America/New_York",
+                "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0,gzip(gfe)",
+                "userInterfaceTheme": "USER_INTERFACE_THEME_DARK",
+                "utcOffsetMinutes": -240,
             },
+            "request": {
+                "consistencyTokenJars": [],
+                "internalExperimentFlags": [],
+            },
+            "user": {
+                "lockedSafetyMode": false
+            }
         },
     },
 };
